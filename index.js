fb46dbacc70921dc0e8fe6b83b9b3b79b4103a79
Roxanne Baldwin <space.legs@Roxannes-MacBook-Air.local>
2017-07-13 15:47:32 -0600

    ✅ Pi (simple endpoint) and Calculate (querystrings)

--- index.js
+++ index.js
@@ -10,4 +10,34 @@ app.get('/math/pi', function(request, response) {
   response.send(Math.PI.toString())
 })
 
+app.get('/math/calculate', function(request, response) {
+  //request.query.variable
+  let operation = request.query.operation
+  let x = Number(request.query.x)
+  let y = Number(request.query.y)
+  let output = x + y
+  let string
+
+  switch(operation) {
+    case 'add' :
+      string = `${x} + ${y} = ${output}`
+      break
+    case 'subtract' :
+      output = x - y
+      string = `${x} - ${y} = ${output}`
+      break
+    case 'multiply' :
+      output = x * y
+      string = `${x} * ${y} = ${output}`
+      break
+    case 'divide' :
+      output = x / y
+      string = `${x} / ${y} = ${output}`
+      break
+    default:
+      string = `${x} + ${y} = ${output}`
+  }
+  response.send(string)
+})
+
 app.listen(PORT)
